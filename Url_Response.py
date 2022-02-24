6807fa5c197ceece36dcf19cde820821f237bd1d
Rishabh Sikarwar <43522937+Cod3Bende4@users.noreply.github.com>
2022-02-24 11:39:05 +0530

    Update Url_Response.py
    Updated

--- Url_Response.py
+++ Url_Response.py
@@ -1,10 +1,13 @@
-# CODE TO CHECK RESPONSE CODE OF A URL WHEN HIT WITH A CURL COMMAND
-
 import subprocess
-args = ("curl -I " + input ("Enter website: ")).split(" ")
+inputs = input ("Enter website: ")
+if "." in inputs:
+  args = ("curl -I " + inputs).split(" ")
 
-out = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-process, err = out.communicate()
+  out = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  process, err = out.communicate()
 
-result = process.decode("utf-8").split("\r\n")[0]
-print (result.split("HTTP/1.1")[1])
+  result = process.decode("utf-8").split("\r\n")[3]
+  print(result)
+else:
+  print("Enter a correct URL")
+  exit()
