81673c695136896636d2e9b2e59d770bb36f09f7
Rishabh Sikarwar <43522937+Cod3Bende4@users.noreply.github.com>
2022-02-24 11:55:09 +0530

    Update Url_Response.py
    Updated

--- Url_Response.py
+++ Url_Response.py
@@ -1,13 +1,17 @@
 import subprocess
 inputs = input ("Enter website: ")
 if "." in inputs:
-  args = ("curl -I " + inputs).split(" ")
+    args = ("curl -I " + inputs).split(" ")
+    out = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    process, err = out.communicate()
 
-  out = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  process, err = out.communicate()
-
-  result = process.decode("utf-8").split("\r\n")[3]
-  print(result)
+    result = process.decode("utf-8").split("\r\n")
+    if "Could not resolve host" in result[2]:
+        print("DNS resolution error \n check if the entered URL is correct")
+        exit()
+    else:
+        print(result[3])
+        exit()
 else:
-  print("Enter a correct URL")
-  exit()
+    print("Enter a correct URL")
+    exit()
